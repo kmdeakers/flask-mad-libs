ae76a41799b493adbd05ae903ecc79e557fae04b
Kevin Deakers <kmdeakers@mac.com>
2023-03-08 17:20:46 -0800

    completed

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get('/')
+def show_questions():
+    """creates form and shows prompts"""
+    prompts = silly_story.prompts
+
+    return render_template(
+        "questions.html",
+        prompts = prompts,
+    )
+
+@app.get('/results')
+def show_results():
+    """Retrieves user input and generates story"""
+    story = silly_story.generate(request.args)
+
+    return render_template(
+        "results.html",
+        text = story
+    )
